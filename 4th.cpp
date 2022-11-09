25a77e3affca3f3933f43cac2ddcd39225a50a1d
Sachin Kumar <109869838+sachinkumar8757@users.noreply.github.com>
2022-11-09 23:35:56 +0530

    Create 4th.cpp
    CPP Program to Multiply Two Floating-Point Numbers

---
+++ 4th.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,mul;
+    cout<<"Enter two number: ";
+    cin>>a>>b;
+    mul=a*b;
+    cout<<"multiplication of "<<a<<" and "<<b<<" is: "<<mul;
+  return 0;
+}
